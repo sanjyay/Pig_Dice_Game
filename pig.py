a419aef549f8102982282eea94ab4ad08fe7fe61
sanjyay <sanjay277@gmail.com>
2024-10-29 09:17:02 +0530

    game function

--- pig.py
+++ pig.py
@@ -3,15 +3,16 @@
 #If they stopped the game before getting 1 then the number of rolls is added to their score
 #if player gets 1 then that game's score is 0
 
+import time
 import random
+dice_list=[1,2,3,4,5,6]    
+
 #the dice will be 1-6
-dice_list=[1,2,3,4,5,6]
-def dice(dice_list):
-    
-    for i in range(len(dice_list)):
-        j=random.randint(0,len(dice_list)-1) #j chooses a random value from 1-6
-        dice_list[i],dice_list[j]=dice_list[j],dice_list[i]
-    
+def dice():
+    dice_list=[1,2,3,4,5,6]    
+    #for i in range(len(dice_list)):
+    j=random.randint(0,len(dice_list)-1) #j chooses a random value from 1-6
+    #    dice_list[i],dice_list[j]=dice_list[j],dice_list[i]
     return dice_list[j]
 
 def players():
@@ -20,19 +21,45 @@ def players():
     for i in range(num_players):
         name_players=input('Enter the names of the players:')
         player_list.append(name_players)
-    return player_list
-
-#to convert players list to dictationary
-def convert(list):
+    #to convert players list to dictationary
     player_dict={}
-    for i in range(0,len(list)):
-        player_dict[list[i]]=0
-    return player_dict
+    for i in range(0,len(player_list)):
+        player_dict[player_list[i]]=0
+    return player_dict,player_list
 
+#game
+def game():
+    for i in range(len(player_list)):
+        print('Player',player_list[i],'will start the game')
+        time.sleep(3)
+        while True:
+            
+            a=dice()
+
+            player_dict[list(player_dict.keys())[i]]+=a 
+
+            print(a)
+            query=input('Do you want to continue?(Press q to quit or Enter to continue)')
+            if query=="q":
+                print(player_dict)
+                break
+            time.sleep(1)
+            if a==1:
+                print('Player',player_list[i],'has lost')
+                player_dict[list(player_dict.keys())[i]]=0
+                print(player_dict)
+                time.sleep(3)
+                break  
+
+
+player_dict,player_list=players()
 def main():
-    pl_list=players()
-    pl_dict=convert(pl_list)
-    print('The current standings',pl_dict)
+    
+    
+    print('The current standings',player_dict)
+    game()
     # roll=dice(dice_list)
     # print('The roll',roll)
 main()
+
+
